1f73647bf132a1b692e9f687241dc02abc90e262
Christopher Warner <chriswarnertech@gmail.com>
2019-09-13 09:00:05 -0400

    added mlab uri

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.set("view engine", "handlebars");
 app.use(routes);
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://<dbuser>:<dbpassword>@ds027483.mlab.com:27483/heroku_sps4hxcn";
 
 // Connect to the Mongo DB
 mongoose.connect(MONGODB_URI);
